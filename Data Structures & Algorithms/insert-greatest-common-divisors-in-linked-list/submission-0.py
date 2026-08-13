c5f8133e83b893d04daae31525668261dce7722d
christianmonty <139390373+christianmonty@users.noreply.github.com>
2026-08-12 21:29:22 -0700

    Add: insert-greatest-common-divisors-in-linked-list - submission-0

---
+++ Data Structures & Algorithms/insert-greatest-common-divisors-in-linked-list/submission-0.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        # brute force, for each set of adjacent pairs
+        # pick smaller, try to divide large by smaller, then
+        # since node.val <= 1000 try small - 1 all the way down
+        # could do euclid's algorithm too for more efficient search
+
+        # maybe we do a stock (gcd) function decomposed. could do math.gcd()
+        # new array is size n-1 O(n) space
+        # first calculate the GCD's, then populate into original list
+
+        def gcd(a: int, b: int) -> int:
+            # find gcd of (a,B) via Euclid's algorithm
+            while b != 0:
+                a, b = b, a % b
+            return abs(a)
+
+        temp = head
+        gcdlist = []
+        while temp and temp.next:
+            a = temp.val
+            b = temp.next.val
+            gcdval = gcd(a,b)
+            gcdlist.append(gcdval)
+            temp = temp.next
+        
+        temp = head
+        i = 0
+        while i < len(gcdlist):
+            aNode = temp
+            bNode = temp.next
+            aNode.next = ListNode(gcdlist[i], bNode)
+            i += 1
+            temp = bNode
+        
+        return head
+
+
+        
