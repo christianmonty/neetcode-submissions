5601d632c4d581a20e79b40287bf6899352dc131
christianmonty <139390373+christianmonty@users.noreply.github.com>
2026-08-28 10:25:12 -0700

    Add: coin-change-ii - submission-9

---
+++ Data Structures & Algorithms/coin-change-ii/submission-9.py
@@ -0,0 +1,80 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        count = 0
+        dp = [[0 for _ in range(len(coins) + 1)] for _ in range(amount + 1)] # double check if need these
+
+        # finally got it to work with this type of recursion!!
+        def recurse(sum: int, index: int) -> int:
+            if index >= len(coins) or sum > amount:
+                return 0
+            if sum == amount:
+                return 1
+            if dp[sum][index+1]:
+                ret1 = dp[sum][index + 1]
+            else:
+                ret1 = recurse(sum, index + 1)
+                if ret1:
+                    dp[sum][index + 1] = ret1
+            
+            if sum + coins[index] <= amount and dp[sum + coins[index]][index]:
+                ret2 = dp[sum + coins[index]][index]
+            else:
+                ret2 = recurse(sum + coins[index], index)
+                if ret2:
+                    dp[sum + coins[index]][index] = ret2
+            return ret1 + ret2
+
+        return recurse(0, 0)
+
+        # this approach didn't work, wasn't efficient enough...
+
+        # idea to memoize, if sum is 2, and already tried at index 2, then use that affect to increase count without another recursive call
+        '''
+        def recurse(sum: int, index: int) -> int:
+            if sum > amount:
+                return 0
+            if sum == amount:
+                return 1
+            count = 0
+            while index < len(coins):
+                if not dp[sum][index]:
+                    # only now recurse
+                    if sum + coins[index] <= amount:
+                        retval = recurse(sum + coins[index], index)
+                        dp[sum + coins[index]][index] = retval
+                        count += retval
+                index += 1
+            return count
+
+        return recurse(0, 0)
+
+        '''
+
+
+        # Below approach is me trying to do a proper 2D DP array vs. recursive solution at each step
+        # my instinct is coins as rows, amount as cols
+        # 1 is always 1
+        # then a cell is top + left + if amount % coin == 0. Or maybe just left + amount % coin == 0
+        # or it's value - coin == thing, check dp of that or something right since can use unlimited of eqach coin
+        # combination since order doesn't matter
+        # there's some order here even I can't yet quite articulate & grasp
+        # then total ways for each value is sum of entire column for that value
+
+        '''
+        0 1 2 3 4
+        1 1 1 1 1
+        2 0 1 1 2
+        3 0 0 1 1
+        T 1 2 3 4
+        '''
+
+        # first fill row with 1
+        # then fill next row starting from coin >= the value
+        # drag from right, add on 1 if value % coin == 0 (besides for 1 row)
+        # then total is accumulation of that value across coins?
+
+        # dp = [[0 for _ in range(amount)] for _ in range(len(coins))] # col inside, row outside
+        # do we need to add a zero row or zero col?
+
+
+        
