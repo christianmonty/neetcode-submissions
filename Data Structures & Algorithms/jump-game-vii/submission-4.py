fcf5627fdf296b0b0301d9ebf18a514589d3c093
christianmonty <139390373+christianmonty@users.noreply.github.com>
2026-09-04 11:45:05 -0700

    Add: jump-game-vii - submission-4

---
+++ Data Structures & Algorithms/jump-game-vii/submission-4.py
@@ -0,0 +1,52 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+
+        # is this DP for the paths, and then greedy for furthest path?
+        goal = len(s) - 1
+        if s[-1] != '0' or s[0] != '0':
+            return False # trivially
+        
+        dp = [False for _ in range(len(s))]
+        dp[len(s) - 1] = True
+
+        for i in range(len(s) - 2, -1, -1):
+            if s[i] == '0':
+                jumpLimit = min(len(s) - 1, i + maxJump)
+                for j in range(i + minJump, jumpLimit + 1):
+                    if dp[j] == True and s[j] == '0':
+                        dp[i] = True
+                        break
+        
+        return dp[0]
+
+
+
+
+        '''
+        # below is my first attempt
+
+        # since greedy, makes me think you always want to take maxjump, since can always scale down if needed
+
+        # WTF is this not greedy, but DP??!?
+
+        i = 0
+        while i < len(s) - 1:
+            j = min(i + maxJump, len(s) - 1)
+            if j - i < minJump:
+                return False # issue here cwhen don't have access to other jumps!!
+            while j >= i + minJump:
+                if j == len(s) - 1:
+                    if s[j] == '1':
+                        return False
+                    else:
+                        return True
+                elif s[j] == '0':
+                    i = j
+                    break
+                elif s[j] == '1' and j == i + minJump:
+                    return False # no more jump to be made!
+                else:
+                    j -= 1
+        return True # means i reached the end
+
+        '''
