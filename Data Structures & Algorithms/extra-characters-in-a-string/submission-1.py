65ddf8dfada7fd51ef91fa71ac5c393c9e7983a0
christianmonty <139390373+christianmonty@users.noreply.github.com>
2026-09-18 21:47:24 -0400

    Add: extra-characters-in-a-string - submission-1

---
+++ Data Structures & Algorithms/extra-characters-in-a-string/submission-1.py
@@ -0,0 +1,81 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.isWord = True
+
+# above is typical way to set up a Trie
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        # want to extend words to longest as pos to minimize extra characters
+        # but greedy may not work vs. bunch of small words
+        # so might as well check dp, skip letter vs. include?
+        # how do we incorporate a Trie here?
+
+        dp = [0 for _ in range(len(s)+1)]
+        i = len(s) - 1
+        words = Trie()
+        for word in dictionary:
+            words.addWord(word)
+        # words = set(dictionary) # convert for O(1) lookup time
+
+        while i >= 0:
+            j = i
+            dp[i] = 1 + dp[i + 1] # skip letter entirely
+            curr = words.root
+            while j < len(s):
+                if s[j] not in curr.children:
+                    break
+                curr = curr.children[s[j]]
+                if curr.isWord:
+                    dp[i] = min(dp[i], dp[j + 1])
+                j += 1
+            i -= 1
+        
+        return dp[0]
+
+
+
+
+
+
+        '''
+        # below is solved DP way, but point of this exercise is practice Tries
+
+        # the dp way to think about it is for dp[i], I can check dp[i+1] + skip dp[i] return 1 + dp[i+1]
+        # or I can take min of dp[i:j] + dp[j+1:end] for all j
+        # or rather, if s[i:j] in dict, take min of dp[j] that fits
+        # or try skipping and that's 1 + dp[i + 1]
+        # set dp[i] to min found from there by either skipping or taking
+
+        dp = [0 for _ in range(len(s)+1)]
+        dp[len(s)] = 0
+        i = len(s) - 1
+        end = len(s)
+        words = set(dictionary) # convert for O(1) lookup time
+
+        while i >= 0:
+            j = i + 1
+            mmin = float('inf')
+            while j < len(s) + 1:
+                if s[i:j] in words:
+                    mmin = min(mmin, dp[j])
+                j += 1
+            mmin = min(mmin, 1 + dp[i + 1]) # skip letter entirely
+            dp[i] = mmin
+            i -= 1
+        
+        return dp[0]
+        '''
